ad9fa6a4936f13bd843c057ed3b954a446e57a08
Jens Krueger <jens.krueger@frm2.tum.de>
2009-09-11 10:42:34 +0000

    control interface for TACO
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@102 71953026-4e7b-de11-b703-00e01807f32f

---
+++ tacocontrol.cpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Gregor Montermann <g.montermann@mesytec.com>    *
+ *   Copyright (C) 2009 by Jens Krüger <jens.krueger@frm2.tum.de>          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "tacocontrol.h"
+#include "mesydaq2.h"
+#include "mdefines.h"
+#include "tacothread.h"
+#include <API.h>
+
+TACOControl::TACOControl(QObject *parent)
+	: ControlInterface(parent)
+{
+	m_tt = new TACOThread(this);
+	m_tt->start();
+}
+
+TACOControl::~TACOControl()
+{
+	if (m_tt->isRunning())
+		m_tt->quit();
+}
+
