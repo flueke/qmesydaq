a07579bf9726bc3e5d72a27fc50c471ef6c6819a
Jens Krueger <jens.krueger@frm2.tum.de>
2009-09-11 10:44:21 +0000

    control interface for TACO
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@105 71953026-4e7b-de11-b703-00e01807f32f

---
+++ MesyDAQDetectorDetector.cpp
@@ -0,0 +1,157 @@
+// Interface to the QMesyDAQ software
+// Copyright (C) 2009 Jens Krüger
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+// TACODEVEL CODEGEN INCLUDES BEGIN
+// This is an automatically generated block.  Do not edit it.  Any modification may be lost.
+
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif // HAVE_CONFIG_H
+
+#include <TACOConverters.h>
+#include <TACOCommands.h>
+
+#include "MesyDAQDetectorDetector.h"
+
+#include <Admin.h>
+#include <iostream>
+
+// TACODEVEL CODEGEN INCLUDES END
+
+	// TACODEVEL CODEGEN BASE CLASS CONSTRUCTOR CALLS BEGIN
+	// This is an automatically generated block.  Do not edit it.  Any modification may be lost.
+
+MesyDAQ::Detector::Detector::Detector( const std::string& name, DevLong& error) throw (::TACO::Exception)
+	: ::TACO::Server( name, error)
+
+	// TACODEVEL CODEGEN BASE CLASS CONSTRUCTOR CALLS END
+
+	, m_pInt(NULL)
+
+	/* , MyFirstBaseClass(), MySecondBaseClass(), ... */
+{
+	// TACODEVEL CODEGEN CONSTRUCTOR CODE BEGIN
+	// This is an automatically generated block.  Do not edit it.  Any modification may be lost.
+
+	addDeviceType( ::Detector::DETECTOR_ID);
+	addCommand(::TACO::Command::START, &tacoStart, D_VOID_TYPE, D_VOID_TYPE, WRITE_ACCESS);
+	addCommand(::TACO::Command::STOP, &tacoStop, D_VOID_TYPE, D_VOID_TYPE, WRITE_ACCESS);
+	addCommand(::TACO::Command::SET_PRESELECTION_DOUBLE, &tacoSetPreselection, D_DOUBLE_TYPE, D_VOID_TYPE, WRITE_ACCESS);
+	addCommand(::TACO::Command::RESUME, &tacoResume, D_VOID_TYPE, D_VOID_TYPE, WRITE_ACCESS);
+	addCommand(::TACO::Command::CLEAR, &tacoClear, D_VOID_TYPE, D_VOID_TYPE, WRITE_ACCESS);
+	addCommand(::TACO::Command::PRESELECTION_DOUBLE, &tacoPreselection, D_VOID_TYPE, D_DOUBLE_TYPE, READ_ACCESS);
+	addCommand(::TACO::Command::READ_U_LONG_ARRAY, &tacoRead, D_VOID_TYPE, D_VAR_ULONGARR, READ_ACCESS);
+
+	setDeviceVersion( VERSION );
+
+	// TACODEVEL CODEGEN CONSTRUCTOR CODE END
+	// TACODEVEL CODEGEN CONSTRUCTOR FINISH CODE BEGIN
+	// This is an automatically generated block.  Do not edit it.  Any modification may be lost.
+	try
+	{
+		v_Init();
+		if (Server::deviceState() != ::TACO::State::INIT)
+			setDeviceState(::TACO::State::DEVICE_NORMAL);
+		logStream->noticeStream() << GetClassName() << " : " << deviceName() << " : init complete." << log4cpp::eol;
+	}
+	catch (const ::TACO::Exception &e)
+	{
+		logStream->fatalStream() << GetClassName() << " : " << deviceName() << " : init failed. " << e.what() << log4cpp::eol;
+		Server::setDeviceState(DEVON_NOT_REACHED);
+	}
+	// TACODEVEL CODEGEN CONSTRUCTOR FINISH CODE END
+}
+
+MesyDAQ::Detector::Detector::~Detector() throw ()
+{
+	// VOID
+}
+
+// TACODEVEL CODEGEN TACO METHOD DEFINITIONS BEGIN
+// This is an automatically generated block.  Do not edit it.  Any modification may be lost.
+
+void MesyDAQ::Detector::Detector::tacoStart( ::TACO::Server* server, DevArgument argin, DevArgument argout) throw (::TACO::Exception)
+{
+	Detector* s = dynamic_cast<Detector*>( server);
+	if (s != 0) {
+		s->start();
+	} else {
+		throw ::TACO::Exception( ::TACO::Error::INTERNAL_ERROR, "bad dynamic cast");
+	}
+}
+
+void MesyDAQ::Detector::Detector::tacoStop( ::TACO::Server* server, DevArgument argin, DevArgument argout) throw (::TACO::Exception)
+{
+	Detector* s = dynamic_cast<Detector*>( server);
+	if (s != 0) {
+		s->stop();
+	} else {
+		throw ::TACO::Exception( ::TACO::Error::INTERNAL_ERROR, "bad dynamic cast");
+	}
+}
+
+void MesyDAQ::Detector::Detector::tacoSetPreselection( ::TACO::Server* server, DevArgument argin, DevArgument argout) throw (::TACO::Exception)
+{
+	Detector* s = dynamic_cast<Detector*>( server);
+	if (s != 0) {
+		s->setPreselection( ::TACO::convert( static_cast<DevDouble*>( argin)));
+	} else {
+		throw ::TACO::Exception( ::TACO::Error::INTERNAL_ERROR, "bad dynamic cast");
+	}
+}
+
+void MesyDAQ::Detector::Detector::tacoResume( ::TACO::Server* server, DevArgument argin, DevArgument argout) throw (::TACO::Exception)
+{
+	Detector* s = dynamic_cast<Detector*>( server);
+	if (s != 0) {
+		s->resume();
+	} else {
+		throw ::TACO::Exception( ::TACO::Error::INTERNAL_ERROR, "bad dynamic cast");
+	}
+}
+
+void MesyDAQ::Detector::Detector::tacoClear( ::TACO::Server* server, DevArgument argin, DevArgument argout) throw (::TACO::Exception)
+{
+	Detector* s = dynamic_cast<Detector*>( server);
+	if (s != 0) {
+		s->clear();
+	} else {
+		throw ::TACO::Exception( ::TACO::Error::INTERNAL_ERROR, "bad dynamic cast");
+	}
+}
+
+void MesyDAQ::Detector::Detector::tacoPreselection( ::TACO::Server* server, DevArgument argin, DevArgument argout) throw (::TACO::Exception)
+{
+	Detector* s = dynamic_cast<Detector*>( server);
+	if (s != 0) {
+		::TACO::assign( static_cast<DevDouble*>( argout), s->preselection());
+	} else {
+		throw ::TACO::Exception( ::TACO::Error::INTERNAL_ERROR, "bad dynamic cast");
+	}
+}
+
+void MesyDAQ::Detector::Detector::tacoRead( ::TACO::Server* server, DevArgument argin, DevArgument argout) throw (::TACO::Exception)
+{
+	Detector* s = dynamic_cast<Detector*>( server);
+	if (s != 0) {
+		::TACO::assign( static_cast<DevVarULongArray*>( argout), s->read());
+	} else {
+		throw ::TACO::Exception( ::TACO::Error::INTERNAL_ERROR, "bad dynamic cast");
+	}
+}
+
+// TACODEVEL CODEGEN TACO METHOD DEFINITIONS END
