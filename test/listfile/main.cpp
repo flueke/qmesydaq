11eb6085b5241322950a581ac75a7e6fbfc1ab97
Jens Krueger <jens.krueger@frm2.tum.de>
2009-10-06 15:00:25 +0000

    add test for acquire into a list mode file
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@165 71953026-4e7b-de11-b703-00e01807f32f

---
+++ test/listfile/main.cpp
@@ -0,0 +1,50 @@
+#include <QCoreApplication>
+#include <QTimer>
+#include <QDebug>
+
+#include <iostream>
+#include <mcpd8.h>
+#include <mesydaq2.h>
+#include <mdefines.h>
+
+int main(int argc, char **argv)
+{
+	QString ip[] = {"192.168.168.121", "192.168.168.122", };	
+	quint16 port[] = {54321, 54321, };
+	QString sourceIP[] = {"192.168.168.5", "192.168.168.5", };
+
+	DEBUGLEVEL = NOTICE;
+
+	QCoreApplication app(argc, argv);
+
+	Mesydaq2 *mesy = new Mesydaq2();
+		
+	for (int i = 0; i < 2; i++)
+		mesy->addMCPD(i, ip[i], port[i], sourceIP[i]);
+
+	mesy->stop();
+
+	mesy->setTimingSetup(0, true, true);
+	mesy->setGain(0, 0, 8, float(1.0));
+	mesy->setThreshold(0, 0, quint16(20));
+	mesy->setPulser(0, 0, 0, MIDDLE, 50, true);
+
+	mesy->setTimingSetup(1, false, true);
+	mesy->setGain(1, 7, 8, float(1.0));
+	mesy->setThreshold(1, 7, quint16(20));
+	mesy->setPulser(1, 7, 0, MIDDLE, 50, true);
+
+	mesy->acqListfile(true);
+	mesy->setListfilename("test.mdat");
+
+	mesy->start();
+	QTimer::singleShot(500, mesy, SLOT(stop()));
+
+	QTimer::singleShot(500, &app, SLOT(quit()));
+
+	app.exec();
+
+	delete mesy;
+
+	return 0;
+}
