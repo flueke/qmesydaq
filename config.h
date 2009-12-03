bfc4718cf45b1b20c7f131b68de4efc6486e8caa
Jens Krueger <jens.krueger@frm2.tum.de>
2009-12-03 16:00:00 +0000

    add 64 bit support
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@306 71953026-4e7b-de11-b703-00e01807f32f

--- config.h
+++ config.h
@@ -1,7 +1,22 @@
 #define HAVE_RPC_RPC_H 1
 #define HAVE_RPC_PMAP_CLNT_H 1
 
+#ifdef HAVE_BIT64
+
+#define SIZEOF_UNSIGNED_LONG 8
+#define SIZEOF_LONG 8
+
+#define SIZEOF_UNSIGNED_INT 4
+#define SIZEOF_INT 4
+
+#else
+
 #define SIZEOF_UNSIGNED_LONG 4
 #define SIZEOF_LONG 4
+
+#define SIZEOF_UNSIGNED_INT 4
+#define SIZEOF_INT 4
+
+#endif
 
 #define HAVE_DECL_GETHOSTNAME 1
