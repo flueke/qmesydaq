bf89806ade1a49a2664331900b737d1989de76d6
Jens Krueger <jens.krueger@frm2.tum.de>
2010-04-28 07:05:42 +0000

    CHANGE : - copyright informations
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@325 71953026-4e7b-de11-b703-00e01807f32f

--- interfaces/taco/MesyDAQDetectorDetectorImpl.cpp
+++ interfaces/taco/MesyDAQDetectorDetectorImpl.cpp
@@ -1,5 +1,5 @@
 // Interface to the QMesyDAQ software
-// Copyright (C) 2009 Jens Krüger
+// Copyright (C) 2009-2010 Jens Krüger
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
