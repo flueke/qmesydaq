fed4630b02b725c44480b01eca5b826e8cafb76b
Jens Krueger <jens.krueger@frm2.tum.de>
2011-06-15 07:50:04 +0000

    Put the IP address in a separate widget.
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@371 71953026-4e7b-de11-b703-00e01807f32f

---
+++ qmesydaq/ipaddresswidget.cpp
@@ -0,0 +1,40 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Jens Krüger <jens.krueger@frm2.tum.de>     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "ipaddresswidget.h"
+#include <QRegExp>
+#include <QRegExpValidator>
+
+IPAddressWidget::IPAddressWidget(const QString &address, QWidget *parent)
+	: QLineEdit(address, parent)
+{
+	QRegExp ex("(([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])");
+	setInputMask("000.000.000.000;0");
+	setValidator(new QRegExpValidator(ex, this));
+	setText(address);
+}
+
+IPAddressWidget::IPAddressWidget(QWidget *parent)
+	: QLineEdit(parent)
+{
+	QRegExp ex("(([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])");
+	setInputMask("000.000.000.000;0");
+	setValidator(new QRegExpValidator(ex, this));
+	setText("0.0.0.0");
+}
