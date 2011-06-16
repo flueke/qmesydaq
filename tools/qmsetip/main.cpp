c75c991d5179e5656b8558919fdd3cb5afbe10e2
Jens Krueger <jens.krueger@frm2.tum.de>
2011-06-16 07:26:08 +0000

    Change the command line parameter handling. Add the new IP address to the output.
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@375 71953026-4e7b-de11-b703-00e01807f32f

--- tools/qmsetip/main.cpp
+++ tools/qmsetip/main.cpp
@@ -1,6 +1,7 @@
 #include <QCoreApplication>
 #include <QTimer>
 #include <QDebug>
+#include <QStringList>
 
 #include <iostream>
 #include <mcpd8.h>
@@ -9,34 +10,46 @@
 
 int main(int argc, char **argv)
 {
-	DEBUGLEVEL = NOTICE;
+	DEBUGLEVEL = FATAL /* NOTICE */;
 
 	QCoreApplication app(argc, argv);
 
 	QString fromIP = "192.168.168.121";
 	QString toIP = "192.168.168.122";
 	int 	id = 0;
+	qint16	ipCount = 0;
+	
+	QStringList args = app.arguments();
+	if (args.size() > 1)
+	{
+		for (int i = 1; i < args.size(); ++i)
+			if (args.at(i) == "-h")
+			{
+				qDebug() << argv[0] << ": [from ipadress [default=192.168.168.121]] [to ipadress [default=192.168.168.122]] [module id [default=0]]";
+				return 1;
+			}
+			else if (args.at(i).count('.'))	// may be ip address
+			{
+				if (!ipCount)
+					fromIP = args.at(i);
+				else
+					toIP = args.at(i);
+				++ipCount;
+			}
+			else
+				id = args.at(i).toInt();
+	}
 
-	if (argc > 1)
+	if (ipCount == 1)
 	{
-		if (QString(argv[1]) == "-h")
-		{
-			qDebug() << argv[0] << ": [from ipadress [default=192.168.168.121]] [to ipadress [default=192.168.168.122]] [module id [default=0]]";
-			return 1;
-		}
-		toIP = argv[1];
+		toIP = fromIP;
+		fromIP = "192.168.168.121";
 	}
-	if (argc > 2)
-	{
-		toIP = argv[2];
-		fromIP = argv[1];
-	}
-	if (argc > 3)
-		id = QString(argv[3]).toInt();
 
 	MCPD8 *m = new MCPD8(id, NULL, fromIP);
 
-	qDebug() << QObject::tr("module 2 : %1").arg(m->version());
+	qDebug() << QObject::tr("%2 module : %1 (id=%3)").arg(m->version()).arg(m->ip()).arg(id);
+	qDebug() << QObject::tr("new IP address : %1").arg(toIP);
 
 	m->setProtocol(toIP);
 
