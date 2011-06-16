2fa34d5a56abb8c4ae9b87c36aff418e7021997e
Jens Krueger <jens.krueger@frm2.tum.de>
2011-06-16 06:59:28 +0000

    Change the parameter handling. Add the module id to the output.
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@373 71953026-4e7b-de11-b703-00e01807f32f

--- tools/qmlmod/main.cpp
+++ tools/qmlmod/main.cpp
@@ -1,6 +1,7 @@
 #include <QCoreApplication>
 #include <QTimer>
 #include <QDebug>
+#include <QStringList>
 
 #include <iostream>
 #include <mcpd8.h>
@@ -16,20 +17,23 @@ int main(int argc, char **argv)
 	QString ip = "192.168.168.121";
 	int	id = 0;
 
-	if (argc > 1)
+	QStringList args = app.arguments();
+	if (args.size() > 1)
 	{
-		if (QString(argv[1]) == "-h")
-		{
-			qDebug() << argv[0] << ": [ipadress [default=192.168.168.121]] [module id [default=0]]";
-			return 1;
-		}
-		ip = argv[1];
+		for (int i = 1; i < args.size(); ++i)
+			if (args.at(i) == "-h")
+			{
+				qDebug() << argv[0] << ": [ipadress [default=192.168.168.121]] [module id [default=0]]";
+				return 1;
+			}
+			else if (args.at(i).count('.'))	// may be ip address
+				ip = args.at(i);
+			else
+				id = args.at(i).toInt();
 	}
-	if (argc > 2)
-		id = QString(argv[2]).toInt();
 
 	MCPD8 *m = new MCPD8(id, NULL, ip);
-	qDebug() << QObject::tr("%2 : MCPD : %1").arg(m->version()).arg(ip);
+	qDebug() << QObject::tr("%2 : MCPD : %1 (id=%3)").arg(m->version()).arg(m->ip()).arg(id);
 
 	for (int i = 0; i < 8; ++i)
 		qDebug() << QObject::tr("module %1 : %2 %3").arg(i + 1).arg(m->getMpsdId(i)).arg(m->version(i));
