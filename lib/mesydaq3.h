7da6c37c6e73bee155a7650f69d3ccc9c1d8ccee
Jens Krueger <jens.krueger@frm2.tum.de>
2011-09-05 15:38:57 +0000

    dummy.
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@445 71953026-4e7b-de11-b703-00e01807f32f

---
+++ lib/mesydaq3.h
@@ -0,0 +1,49 @@
+#if 0
+mdll.cpp: In member function 'void Mdll::setMdll(_MDLL_SETTINGS*, bool)':
+mdll.cpp:268: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setThreshold()':
+mdll.cpp:351: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setSpectrum()':
+mdll.cpp:365: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setDatareg()':
+mdll.cpp:412: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setAcqset()':
+mdll.cpp:428: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setEnergy()':
+mdll.cpp:442: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setCountercells()':
+mdll.cpp:456: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setAuxtimers()':
+mdll.cpp:470: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setParams()':
+mdll.cpp:484: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::setTiming()':
+mdll.cpp:503: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+mdll.cpp: In member function 'void Mdll::serialize(QFile*)':
+mdll.cpp:539: error: invalid use of incomplete type 'struct mesydaq3'
+mdll.h:32: error: forward declaration of 'struct mesydaq3'
+#endif
+
+#ifndef MESYDAQ3_H
+#define MESYDAQ3_H
+class NetworkDevice;
+
+class mesydaq3
+{
+public:
+	void sendBuffer(quint8 id, quint8 len){}
+
+	NetworkDevice *netDev;
+};
+
+#endif
