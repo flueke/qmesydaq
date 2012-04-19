d6e18f1c53b09ac01976ca43860cdacbf0779ff6
Jens Krueger <jens.krueger@frm2.tum.de>
2012-04-19 15:32:54 +0000

    remove unused header files
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@487 71953026-4e7b-de11-b703-00e01807f32f

--- qmesydaq/mainwindow.cpp
+++ qmesydaq/mainwindow.cpp
@@ -17,11 +17,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
-#include <QSettings>
-
 #include "MultipleLoopApplication.h"
-
 #include "mainwindow.h"
 #include "mainwidget.h"
 #include "mesydaq2.h"
