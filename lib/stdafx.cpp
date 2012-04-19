14d1978b47a9aedf6f2bff43c9775f4abff368bf
Jens Krueger <jens.krueger@frm2.tum.de>
2012-04-19 16:06:13 +0000

    Prepare compiling with MSVC 10
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@502 71953026-4e7b-de11-b703-00e01807f32f

---
+++ lib/stdafx.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+
+#if defined(_MSC_VER)
+
+#include <QMutex>
+
+int round(double value)
+{
+	if (value > 0)
+		value += 0.5;
+	else
+		value -= 0.5;
+	return (int)value;
+}
+void sleep(unsigned int s)
+{
+	QMutex tmpMutex;
+	tmpMutex.lock();
+	tmpMutex.tryLock(s*1000);
+}
+void usleep(unsigned int us)
+{
+	QMutex tmpMutex;
+	tmpMutex.lock();
+	tmpMutex.tryLock(us/1000);
+}
+#endif
---
+++ lib/stdafx.h
@@ -0,0 +1,25 @@
+#ifndef LIB_QMESYDAQ_STDAFX
+#define LIB_QMESYDAQ_STDAFX
+
+//QT Includes
+//#include <QCoreApplication>
+//#include <QtCore>
+//#include <QtNetwork>
+
+//C++ Includes
+//#include <list>
+//#include <iostream>
+
+//C Includes
+//#include <cstdio>
+//#include <cmath>
+
+#if defined(_MSC_VER)
+//Functions
+	int round(double value);
+//POSIX Functions
+	void sleep(unsigned int s);
+	void usleep(unsigned int us);
+#endif
+
+#endif //LIB_QMESYDAQ_STDAFX
