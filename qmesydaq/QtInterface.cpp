fbf460bc2ce79c8fbc63e1ba207e4437f7378a47
Jens Krueger <jens.krueger@frm2.tum.de>
2012-04-19 15:35:46 +0000

    beautify code
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@490 71953026-4e7b-de11-b703-00e01807f32f

--- qmesydaq/QtInterface.cpp
+++ qmesydaq/QtInterface.cpp
@@ -19,12 +19,10 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
-#include "QtInterface.h"
-
-#include "QApplication"
-#include"LoopObject.h"
+#include <QApplication>
 #include <QDateTime>
+#include "QtInterface.h"
+#include "LoopObject.h"
 
 /*!
     constructor
