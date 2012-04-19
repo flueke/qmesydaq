e647734d84dca2dbf6058934ce811669e72ce9c5
Jens Krueger <jens.krueger@frm2.tum.de>
2012-04-19 15:31:04 +0000

    remove unused header files
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@486 71953026-4e7b-de11-b703-00e01807f32f

--- qmesydaq/MultipleLoopApplication.cpp
+++ qmesydaq/MultipleLoopApplication.cpp
@@ -19,13 +19,10 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
 #include "MultipleLoopApplication.h"
-
 #include "QtInterface.h"
 #include "LoopObject.h"
 
-#include <iostream>
 
 /*!
     constructor
