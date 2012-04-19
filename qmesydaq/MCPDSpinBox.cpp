5ecd365160d16f74358ef9afd97c06bbbf431850
Jens Krueger <jens.krueger@frm2.tum.de>
2012-04-19 15:39:13 +0000

    remove unused header files
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@493 71953026-4e7b-de11-b703-00e01807f32f

--- qmesydaq/MCPDSpinBox.cpp
+++ qmesydaq/MCPDSpinBox.cpp
@@ -16,9 +16,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
 #include "MCPDSpinBox.h"
-#include <QDebug>
 
 /*!
     \fn MCPDSpinBox::MCPDSpinBox(QList<int> modules, QWidget *parent)
