da8cfb53c5808084709f047ed011cbcfd80a1316
Jens Krueger <jens.krueger@frm2.tum.de>
2012-06-12 11:42:22 +0000

    Add timing function for more detailed information.
    Add a timing protocol to get more detailed information about time consumption.
    
    git-svn-id: https://trac.frm2.tum.de/qmesydaq/trunk@595 71953026-4e7b-de11-b703-00e01807f32f

--- test/fill/main.cpp
+++ test/fill/main.cpp
@@ -1,19 +1,37 @@
 #include "../../lib/histogram.cpp"
 
+#include <iostream>
+#include <QTime>
+
+const int events = 10000000;
+
 int main(int, char **)
 {
+	QTime		t;
+
+	t.start();
 	Spectrum 	s;
 
+	std::cerr << "init of s tooks " << t.elapsed() << " ms" << std::endl;
+
+	t.restart();
 	Histogram	h1;
+	std::cerr << "init of h1 tooks " << t.elapsed() << " ms" << std::endl;
+	t.restart();
 	Histogram	h2;
+	std::cerr << "init of h2 tooks " << t.elapsed() << " ms" << std::endl;
 
 	const quint32	hsize = 128 * 960;
 
-	for (int i = 0; i < 10000000; ++i)
+	std::cerr << "fill " << events << " into a spectrum" << std::endl;
+
+	t.restart();
+	for (int i = 0; i < events; ++i)
 	{
-		s.incVal(i % hsize);
+		s.incVal(i % 960);
 		h1.incVal(i % 128, i % 960);
 		h2.incVal(i % 128, i % 960);
 	}
+	std::cerr << "adding of " << events << " events tooks " << t.elapsed() << " ms" << std::endl;
 	return 0;
 }
