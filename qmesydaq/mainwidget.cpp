3a3b0f8eaa529253264d3cb57f0355f32e27270c
Jens Krüger <jens.krueger@frm2.tum.de>
2014-03-18 08:58:27 +0100

    Add authors of the histogram mapping editor to the about info
    Change-Id: I26503c5dd09870f236294586d87a9bfb67f7ade1
    Reviewed-on: https://forge.frm2.tum.de/review/4649
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>

--- qmesydaq/mainwidget.cpp
+++ qmesydaq/mainwidget.cpp
@@ -243,6 +243,8 @@ void MainWidget::about()
 		   + tr("<li><a href=\"mailto:alexander.lenz@frm2.tum.de\">Alexander Lenz</a> TACO remote control</li>")
                    + tr("<li><a href=\"mailto:m.drochner@fz-juelich.de\">Matthias Drochner</a> Bug reports</li>")
                    + tr("<li><a href=\"mailto:christian.randau@frm2.tum.de\">Christian Randau</a> Windows port</li>")
+		   + tr("<li><a href=\"mailto:eric.faustmann@helmholtz-berlin.de\">Eric Faustmann</a>Histogram mapping editor</li>") 
+		   + tr("<li><a href=\"mailto:damian.rhein@helmholtz-berlin.de\">Damian Rhein</a>Histogram mapping editor</li>") 
                    + tr("</ul><p>This program controls the data acquisition and display for the MesyTec MCPD-2/8 modules</p>");
     LoopObject *loop = dynamic_cast<LoopObject *>(dynamic_cast<MultipleLoopApplication*>(QApplication::instance())->getLoopObject());
     if (loop)
