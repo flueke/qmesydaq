1d6edf89af98c942a0f7eca279401da299a729c4
Jens Krüger <jens.krueger@frm2.tum.de>
2019-11-05 18:16:32 +0100

    test/inspect: add routines to simulate package inspection
    Change-Id: Ib8f51c3bfeca1a65ff34d96d863bda949489ed45

---
+++ test/inspect/main.cpp
@@ -0,0 +1,135 @@
+#include "../../lib/structures.h"
+
+#include <iostream>
+#include <QTime>
+
+void results(std::string s, int packets, int elapsed)
+{
+	if (!elapsed)
+		elapsed = 1;
+	std::cerr << s << " inspecting of " << packets * 243 << " events took " << elapsed << " ms" << std::endl;
+	std::cerr << "MEvents/s : " << packets * 243 / elapsed / 1000 << std::endl;
+	// std::cerr << number << " are in " << s << std::endl;
+}
+
+struct event
+{
+	bool trigger;
+	quint16	amplitude;
+	quint16 x;
+	quint16 y;
+	quint64	time;
+}EVENT;
+
+int main(int, char **)
+{
+	QTime		t;
+	const int packets = 400000;
+	struct DATA_PACKET dp;
+	quint64		n(0);
+
+	dp.bufferLength = 750;
+	dp.bufferType = 1;
+	dp.headerLength = 21;
+	dp.bufferNumber = 1;
+	dp.runID = 1;
+	dp.deviceStatus = 0;
+	dp.deviceId = 0;
+	for (int i = 0; i < 3; ++i)
+		dp.time[0] = 0;
+	// dp.param[4][3];
+	// dp.data[750];
+
+	t.start();
+	for (int i = 0; i < packets; ++i)
+		n += dp.bufferLength;
+	results("bufferLength", packets, t.elapsed()); 
+
+	std::cerr << n << std::endl;
+
+	quint64 headerTime(0);
+	n = 0;
+	t.start();
+	for (int i = 0; i < packets; ++i)
+	{
+		n += dp.bufferLength;
+		headerTime = (quint64(dp.time[2]) << 32) | (dp.time[1] << 16) | dp.time[0];
+	}
+	results("headerTime", packets, t.elapsed()); 
+
+	n = 0;
+	t.start();
+	for (int i = 0; i < packets; ++i)
+	{
+		n += dp.bufferLength;
+		headerTime = (quint64(dp.time[2]) << 32) | (dp.time[1] << 16) | dp.time[0];
+		quint16 nevents = (dp.bufferLength - dp.headerLength) / 3;
+
+		for (int j = 0; j < nevents; ++j)
+		{
+			int k = j * 3;
+			quint64 event = (quint64(dp.data[k + 2]) << 32) | dp.data[k + 1] << 16 | dp.data[k];
+		}
+	}
+	results("events", packets, t.elapsed()); 
+
+	n = 0;
+	t.start();
+	for (int i = 0; i < packets; ++i)
+	{
+		n += dp.bufferLength;
+		headerTime = (quint64(dp.time[2]) << 32) | (dp.time[1] << 16) | dp.time[0];
+		quint16 nevents = (dp.bufferLength - dp.headerLength) / 3;
+
+		for (int j = 0; j < nevents; ++j)
+		{
+			int k = j * 3;
+			quint64 evt = (quint64(dp.data[k + 2]) << 32) | dp.data[k + 1] << 16 | dp.data[k];
+			struct event ev;
+			ev.trigger = evt & (0x800000);
+			ev.amplitude = (evt >> 39) & 0xFF;
+			ev.x = (evt >> 19) & 0x2FF;
+			ev.y = (evt >> 29) & 0x2FF;
+			ev.time = headerTime + (evt & 0x3FFFF);
+		}
+	}
+	results("inspect events", packets, t.elapsed()); 
+#if 0
+	Spectrum 	s;
+	std::cerr << "init of s took " << t.elapsed() << " ms" << std::endl;
+
+	t.restart();
+	Histogram	h1;
+	std::cerr << "init of h1 took " << t.elapsed() << " ms" << std::endl;
+	t.restart();
+	Histogram	h2;
+	std::cerr << "init of h2 took " << t.elapsed() << " ms" << std::endl;
+
+	t.restart();
+	hist		h3;
+	std::cerr << "init of h3 took " << t.elapsed() << " ms" << std::endl;
+
+	const quint32	hsize = 128 * 960;
+
+	t.restart();
+	for (int i = 0; i < events; ++i)
+		s.incVal(i % 960);
+	results("s", events, t.elapsed(), s.getTotalCounts());
+
+	t.restart();
+	for (int i = 0; i < events; ++i)
+		h1.incVal(i % 960, i % 128);
+	results("h1", events, t.elapsed(), h1.getTotalCounts());
+
+	t.restart();
+	for (int i = 0; i < events; ++i)
+		h2.incVal(i % 960, i % 128);
+	results("h2", events, t.elapsed(), h2.getTotalCounts());
+
+	t.restart();
+	for (int i = 0; i < events; ++i)
+		h3.incVal(i % 960, i % 128);
+	results("h3", events, t.elapsed(), h3.getTotalCounts());
+#endif
+	return 0;
+}
