d2098243ad4bc6fca668fad5369a002545175d10
Jens Krüger <jens.krueger@frm2.tum.de>
2019-11-08 08:37:03 +0100

    Input4: Improve result output
    Change-Id: I70da487aeb8f19c7afec81a416625a92e6e461b2

--- test/input4/main.cpp
+++ test/input4/main.cpp
@@ -5,11 +5,14 @@
 #include "packageprovider.h"
 #include "packagehandler.h"
 
-void results(std::string s, int packets, int elapsed)
+void results(std::string s, int packets, int elapsed, bool evt = true)
 {
 	if (!elapsed)
 		elapsed = 1;
-	std::cerr << s << " inspecting of " << packets * 243 << " events took " << elapsed << " ms" << std::endl;
+	if (evt)
+		std::cerr << s << " of " << packets * 243 << " events took " << elapsed << " ms" << std::endl;
+	else
+		std::cerr << s << " of " << packets << " packets took " << elapsed << " ms" << std::endl;
 	std::cerr << "MEvents/s : " << packets * 243 / elapsed / 1000. << std::endl;
 	// std::cerr << number << " are in " << s << std::endl;
 }
@@ -17,6 +20,8 @@ void results(std::string s, int packets, int elapsed)
 int main(int, char **)
 {
 	int		t;
+
+	quint32 	packets(400000);
 
 	// struct DATA_PACKET orig, copy;
 
@@ -26,35 +31,35 @@ int main(int, char **)
 	QObject::connect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		         &ph, SLOT(analyzeBuffer1(const DATA_PACKET &)));
 	t = pp.run();
-	results("package number", 400000, t);
+	results("get header time", packets, t, false);
 	QObject::disconnect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		         &ph, SLOT(analyzeBuffer1(const DATA_PACKET &)));
 
 	QObject::connect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		         &ph, SLOT(analyzeBuffer2(const DATA_PACKET &)));
 	t = pp.run();
-	results("package time", 400000, t);
+	results("unpack events", packets, t);
 	QObject::disconnect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		         &ph, SLOT(analyzeBuffer2(const DATA_PACKET &)));
 
 	QObject::connect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		         &ph, SLOT(analyzeBuffer3(const DATA_PACKET &)));
 	t = pp.run();
-	results("events", 400000, t);
+	results("analyze events", packets, t);
 	QObject::disconnect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		            &ph, SLOT(analyzeBuffer3(const DATA_PACKET &)));
 
 	QObject::connect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		         &ph, SLOT(analyzeBuffer4(const DATA_PACKET &)));
 	t = pp.run();
-	results("histogram events", 400000, t);
+	results("histogram events", packets, t);
 	QObject::disconnect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		            &ph, SLOT(analyzeBuffer4(const DATA_PACKET &)));
 
 	QObject::connect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		         &ph, SLOT(analyzeBuffer5(const DATA_PACKET &)));
 	t = pp.run();
-	results("all histogram events", 400000, t);
+	results("all histogram events", packets, t);
 	QObject::disconnect(&pp, SIGNAL(bufferReceived(const DATA_PACKET &)),
 		            &ph, SLOT(analyzeBuffer5(const DATA_PACKET &)));
 	return 0;
