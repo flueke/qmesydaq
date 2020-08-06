1b7fdb2afade95bfdaed7737b24a8cf5ec28335b
Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
2020-08-06 07:18:01 +0200

    Drop rst2psdf support
    rst2pdf is not supported upstream and broken on python3. We normally build the latexpdf anyway.
    
    Change-Id: I19b52953e5d5d782d2f868ec004e854d81f9aa7e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/general/qmesydaq/+/23608
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'rst2pdf.pdfbuilder',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
