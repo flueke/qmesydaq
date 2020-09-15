4299b2381b533680309c98b9e37d74a9a959678f
Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
2020-09-15 15:22:21 +0200

    Revert "Drop rst2psdf support"
    This reverts commit c3c1e115635a3b51811e67b24ccf3d6cef38ef7b.
    
    Reason for revert: rst2pdf is active again
    
    Change-Id: I2a5cf66cacd8315161d118a15d544193fa425584
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/general/qmesydaq/+/23852
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
+    'rst2pdf.pdfbuilder',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
